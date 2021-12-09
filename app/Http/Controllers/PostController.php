4479f43db04614a6e9f32616467e7e420b8231ff
ArelSmith <wonderfulofgame@gmail.com>
2021-12-09 18:18:13 +0700

    Adding pagination system

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,15 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
         return view('posts', [
-            "title" => "All Posts",
+            "title" => "All Posts" . $title,
             "active" => "posts",
-            "posts" => Post::latest()->filter(request(['search', 'category']))->get()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -45,19 +45,3 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'title' => "Post By Category: $category->name",
-        "active" => "categories",
-        'posts' => $category->posts->load('author', 'category'),
-    ]);
-});
-
-Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', [
-        'title' => "Post by Author: $author->name",
-        "active" => "authors",
-        'posts' => $author->posts->load('category', 'author'),
-    ]);
-});
